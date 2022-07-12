8511fb0186b6b39db02b3fe61431382ec1fc1301
Cwoodard2 <cawooda@siue.edu>
2022-07-11 20:44:23 -0500

    Added homepage into app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Homepage from './pages/homepage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React, Hey man this is me making an edit!
+    //     </a>
+    //   </header>
+    // </div>
+    <HashRouter>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+        </Routes>
+      </HashRouter>
   );
 }
 
