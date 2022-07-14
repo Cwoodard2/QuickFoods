1e6e07e2225357410fd3d1d99fe2fdb8fff287db
Cwoodard2 <cawooda@siue.edu>
2022-07-13 23:42:42 -0500

    Start of the recipe book

--- src/pages/recipebook.js
+++ src/pages/recipebook.js
@@ -1,11 +1,39 @@
 import React from "react";
 import StandardPage from "../components/StandardPage";
 import Navigation from "../components/Navigation";
+import RecipeCard from "../components/RecipeCard";
+import "./recipebook.css";
 
 export default function RecipeBook() {
     return(
         <StandardPage>
             <Navigation />
+            <div className="recipeBookMain">
+                <h1>Olivia's Recipe Book</h1>
+                <div className="column" style={{height: "85vh", overflowY: "auto", gap: "3vw"}}>
+                    <div className="row" style={{gap: "5vw"}}>
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                    </div>
+                    <div className="row" style={{gap: "5vw"}}>
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                    </div>
+                    <div className="row" style={{gap: "5vw"}}>
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                        <RecipeCard />
+                    </div>
+                    </div>
+                </div>
         </StandardPage>
     );
 }
