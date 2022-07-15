91b54d16666644b7aff2ad69c7958f88e7059e99
Cwoodard2 <cawooda@siue.edu>
2022-07-15 18:24:09 -0500

    Loading recipes works correctly... kinda

--- src/pages/recipebook.js
+++ src/pages/recipebook.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { fillArray } from "../database/firebaseInterface";
 import StandardPage from "../components/StandardPage";
 import Navigation from "../components/Navigation";
 import RecipeCard from "../components/RecipeCard";
@@ -7,6 +8,39 @@ import CreateRecipeModal from "../components/CreateRecipeModal";
 
 export default function RecipeBook(props) {
     const [showCreateModal, setCreate] = useState(false);
+    const [fillRecipes, setFillRecipes] = useState({});
+    const [breakfast, setBreakfast] = useState([]);
+    const [lunch, setLunch] = useState([]);
+    const [dinner, setDinner] = useState([]);
+
+    var breakfastRecipes;
+
+    useEffect(() => {
+        const fetchData = async () => {
+          const list = await fillArray();
+          setFillRecipes(list);
+          setBreakfast(list.BreakfastRecipes);
+        }
+
+        console.log("Fetching Data");
+        fetchData();
+    }, []);
+
+    breakfastRecipes = fillRecipes.BreakfastRecipes;
+
+    console.log(breakfastRecipes);
+    console.log(breakfast);
+    // {() => {
+    //     const breakfastTime = fillRecipes.BreakfastRecipes.map(recipes => <li><RecipeCard recipe={recipes.Name} prep={recipes.PrepTime} cook={recipes.CookTime} content={recipes.Description}/></li>);
+    //     return breakfastTime;
+    //     }};
+
+        function makeList() {
+            const breakfastTime = breakfast.map((recipes) => <li><RecipeCard recipe={recipes.Name} prep={recipes.PrepTime} cook={recipes.CookTime} content={recipes.Description}/></li>);
+            return breakfastTime;
+        };
+
+        const thisBreakfast = makeList();
 
     return(
         <StandardPage>
@@ -21,11 +55,14 @@ export default function RecipeBook(props) {
                     <div className="column">
                         <h1>Breakfast Recipes</h1>
                         <div className="row" style={{gap: "5vw", paddingLeft: "2vw", paddingRight: "2vw", paddingTop: "1vw", paddingBottom: "2vw"}}>
-                            <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/>
+                            {/* <RecipeCard recipe={fillRecipes.BreakfastRecipes[0].Name} prep={fillRecipes.BreakfastRecipes[0].PrepTime} cook={fillRecipes.BreakfastRecipes[0].CookTime} content={fillRecipes.BreakfastRecipes[0].Description}/>
                             <RecipeCard recipe="recipe2" prep="5 min" cook="5min" content="This is recipe 2"/>
                             <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/>
                             <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/>
-                            <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/>
+                            <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/> */}
+                            <ul>
+                                {thisBreakfast}
+                            </ul>
                         </div>
                     </div>
                     <div className="column">
