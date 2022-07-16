2f44b20ca1f84c0eb698364cb6e9bbfd7a5c809c
Cwoodard2 <cawooda@siue.edu>
2022-07-16 00:12:46 -0500

    Looks like the bug is related to the nested data in database Unsure how to fix but working on it

--- src/components/GroceryListCat.js
+++ src/components/GroceryListCat.js
@@ -1,15 +1,54 @@
 import React, { useEffect, useState } from "react";
+import { fillArray } from "../database/firebaseInterface";
 
 export default function GroceryListCat(props) {
     var category = props.whichCat;
     var catItems = props.catItems;
     // console.log(props.catItems);
-    const [items, setValue] = useState(props.catItems);
+    const [items, setValue] = useState([]);
 
     // if (category === "Dairy") {
     //     setValue(catItems.Dairy);
     // }
     // const [items, setValue] = useState(props.catItems);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          var list = await fillArray();
+          setValue(list.GroceryList.Dairy);
+        };
+        
+        console.log("Fetching Data");
+        fetchData();
+      }, []);
+
+      console.log(items);
+
+    //   switch(category) {
+    //     case "Dairy":
+    //         setValue(items.Dairy);
+    //         break;
+    //     case "Vegetables":
+    //         setValue(items.Vegetables);
+    //         break;
+    //     case "Fruit":
+    //         setValue(items.Fruit);
+    //         break;
+    //     case "Snacks":
+    //         setValue(items.Snacks);
+    //         break;
+    //     case "Bread":
+    //         setValue(items.Bread);
+    //         break;
+    //     case "Protein":
+    //         setValue(items.Protein);
+    //         break;
+    //     case "Random":
+    //         setValue(items.Random);
+    //         break;
+    //     default:
+    //         break;
+    //   }
 
     function addItem() {
         const newList = items.concat(document.getElementById("addBox").value);
