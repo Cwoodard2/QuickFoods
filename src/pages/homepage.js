63df3add1a82bcc2226ef9e23698b754d4c0050f
Cwoodard2 <cawooda@siue.edu>
2022-07-19 10:03:44 -0500

    small CSS update

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -8,12 +8,24 @@ import RecipeCard from "../components/RecipeCard";
 import "./homepage.scss"
 
 export default function Homepage() {
+    const [mainRecipe1, setRecipe1] = useState();
+    const [mainRecipe2, setRecipe2] = useState();
     var date = new Date();
     var currentTime = date.getHours();
-    console.log(typeof currentTime);
     var mealSuggestions;
     var timeOfDay;
     const {currentUser} = useAuth();
+
+    if (parseInt(currentTime) >= 0 && parseInt(currentTime) <= 11){
+        mealSuggestions = "Breakfast";
+        timeOfDay = "Morning";
+    } else if(parseInt(currentTime) >= 12 && parseInt(currentTime) <= 16) {
+        mealSuggestions = "Lunch";
+        timeOfDay = "Afternoon";
+    } else if(currentTime >= 17 && currentTime <= 23) {
+        mealSuggestions = "Dinner";
+        timeOfDay = "Evening";
+    }
 
     useEffect(() => {
         const checkForDoc = async () => {
@@ -22,9 +34,25 @@ export default function Homepage() {
             const docSnap = await getDoc(docRef);
             if (docSnap.exists()) {
                 console.log("document found");
+                switch(mealSuggestions) {
+                    case "Breakfast":
+                        const data = docSnap.data();
+                        const whichRecipe1 = 0;
+                        console.log("This is recipe 1" + whichRecipe1);
+                        const whichRecipe2 = 0;
+                        setRecipe1(data.BreakfastRecipes[whichRecipe1]);
+                        setRecipe2(data.BreakfastRecipes[whichRecipe2]);
+                        break;
+                    case "Lunch":
+                        break;
+                    case "Dinner":
+                        break;
+                    default:
+                        break;
+                }
             } else {
                 console.log("creating doc");
-                prompt("Welcome to QuickFoods! Since you're a new user we'd like to know your name so you can have a more personalized service! PLease enter it below.");
+                prompt("Welcome to QuickFoods! Since you're a new user we'd like to know your name so you can have a more personalized service! Please enter it below.");
                 const res = await setDoc(doc(db, "Users", currentUser.uid), {
                     BreakfastRecipes: [],
                     DinnerRecipes: [],
@@ -45,17 +73,9 @@ export default function Homepage() {
 
         checkForDoc();
     }, []);
+    console.log(mainRecipe1);
 
-    if (parseInt(currentTime) >= 0 && parseInt(currentTime) <= 11){
-        mealSuggestions = "Breakfast";
-        timeOfDay = "Morning";
-    } else if(parseInt(currentTime) >= 12 && parseInt(currentTime) <= 16) {
-        mealSuggestions = "Lunch";
-        timeOfDay = "Afternoon";
-    } else if(currentTime >= 17 && currentTime <= 23) {
-        mealSuggestions = "Dinner";
-        timeOfDay = "Evening";
-    }
+
     return (
         <StandardPage>
             <Navigation />
@@ -67,8 +87,8 @@ export default function Homepage() {
                     <div className="column">
                         <h2 style={{color: "#FECD45"}}>{mealSuggestions} Suggestions</h2>
                         <div className="recipe-suggestions row">
-                            <RecipeCard recipe="recipe1" prep="0 min" cook="10min" content="This is recipe 1"/>
-                            <RecipeCard recipe="recipe2" prep="5 min" cook="5min" content="This is recipe 2"/>
+                            {/* <RecipeCard recipe={mainRecipe1.Name} prepTime={mainRecipe1.PrepTime} cook={mainRecipe1.CookTime} content={mainRecipe1.Description} instructions={mainRecipe1.Cook} prep={mainRecipe1.Prep}/>
+                            <RecipeCard recipe={mainRecipe2.Name} prepTime={mainRecipe2.PrepTime} cook={mainRecipe2.CookTime} content={mainRecipe2.Description} instructions={mainRecipe2.Cook} prep={mainRecipe2.Prep}/> */}
                         </div>
                     </div>
                     <div className="column">
