a52204ae03ac44889fa84cb5c33df4c4d19ef20e
Cwoodard2 <cawooda@siue.edu>
2022-07-25 23:57:18 -0500

    Improving CSS of the calendar component and small html additions

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -7,13 +7,21 @@ export default function Calendar() {
     var getDate = date.getDate();
     var getMonth = date.getMonth();
 
+    const addRecipe = (whichMeal) => {
+        console.log(whichMeal);
+        return "this is a word";
+    }
+
     return(
         <div>
             <div className="calendar-day">
                 <h3>{getDate} {getDay} {getMonth}</h3>
                 <h4>Breakfast</h4>
+                <button onClick={() => addRecipe("breakfast")}>Add a recipe</button>
                 <h4>Lunch</h4>
+                <button onClick={() => addRecipe("lunch")}>Add a recipe</button>
                 <h4>Dinner</h4>
+                <button onClick={() => addRecipe("dinner")}>Add a recipe</button>
             </div>
         </div>
     )
