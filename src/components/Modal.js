a0b15f65c20092441c6d1cf22fd7d2b181bf9de4
Cwoodard2 <cawooda@siue.edu>
2022-07-27 19:00:18 -0500

    trying to begin framework to be able to edit recipes

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -8,6 +8,13 @@ export default function Modal(props) {
         return null;
     }
 
+    const handleClose = () => {
+        // var instructions = document.getElementById("instructions");
+        // instructions.disabled = false;
+        // return 0;
+        
+    }
+
     if (props.attributes.length == 0) {
         var attributeArray = "None";
     } else {
@@ -24,7 +31,8 @@ export default function Modal(props) {
         <div className="modal">
             <div className="modal-content" style={{overflowY: "auto"}}>
                 <h1>{props.title}</h1>
-                <p style={{whiteSpace: "initial"}}>{props.description}</p>
+                {/* <button onClick={() => handleClick()}>Edit</button> */}
+                <p style={{whiteSpace: "initial"}} contentEditable="true">{props.description}</p>
                 <div className="modal-row modal-content-row">
                 <div className="column modal-content-column">
                     <h3>Ingredients:</h3>
@@ -32,11 +40,12 @@ export default function Modal(props) {
                         {ingredientsArray}
                     </ul>
                     <h3>Cooking Instructions</h3>
-                    <p>{props.instructions}</p>
+                    <p contentEditable="true">{props.instructions}</p>
+                    {/* <input id="instructions" value={props.instructions} disabled style={{width: "auto", height: "auto"}}></input> */}
                 </div>
                 <div className="column modal-content-column">
-                    <h3>Prep Time: {props.prepTime}</h3>
-                    <h3>Cook Time: {props.cookTime}</h3>
+                    <h3>Prep Time: <div contentEditable="true">{props.prepTime}</div></h3>
+                    <h3>Cook Time: <div contentEditable="true">{props.cookTime}</div></h3>
                     <h3>Attributes:</h3>
                     <div className="row" style={{gap: "0.5vw"}}>
                         {attributeArray}
