3bcf777ac0e14ad105bc36564340b4ea2edd2c8c
Cwoodard2 <cawooda@siue.edu>
2022-07-28 00:09:18 -0500

    Added edit and view buttons to encourage editing. Still working on logic to actually update the info

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+import {useAuth} from "../database/authContext";
+import { updateDoc, getDoc, doc } from "firebase/firestore";
 import "./Modal.scss";
 import "../pages/homepage.scss";
 
 export default function Modal(props) {
+    const [editable, setEditable] = useState("false");
+    const [viewMode, setViewMode] = useState("view-on");
+    const [editMode, setEditMode] = useState("edit-off");
+
+    const {currentUser} = useAuth();
 
     if (!props.show) {
         return null;
     }
 
-    const handleClose = () => {
-        // var instructions = document.getElementById("instructions");
-        // instructions.disabled = false;
-        // return 0;
-        
+    const handleSave = () => {
+        const docRef = doc(db, "Users", currentUser.uid);
+        const docSnap = getDoc(docRef);
+        const docData = docSnap.data();
+        const recipeData = docData.BreakfastRecipes;
+        for (var i = 0; i < docData.data(); i++) {
+            if (recipeData[i].Name == props.title) {
+                //save my data
+                break;
+            }
+        }
+    }
+
+    const changeMode = (toMode) => {
+        if (toMode === "view") {
+            setEditable("false");
+            setViewMode("view-on");
+            setEditMode("edit-off");
+        } else if (toMode === "edit") {
+            setEditable("true");
+            setViewMode("view-off");
+            setEditMode("edit-on");
+        }
     }
 
     if (props.attributes.length == 0) {
@@ -31,8 +57,12 @@ export default function Modal(props) {
         <div className="modal">
             <div className="modal-content" style={{overflowY: "auto"}}>
                 <h1>{props.title}</h1>
+                <div className="row" style={{padding: "2vw"}}>
+                    <button onClick={() => changeMode("view")} className={viewMode}>View</button>
+                    <button onClick={() => changeMode("edit")} className={editMode}>Edit</button>
+                </div>
                 {/* <button onClick={() => handleClick()}>Edit</button> */}
-                <p style={{whiteSpace: "initial"}} contentEditable="true">{props.description}</p>
+                <p id="description" style={{whiteSpace: "initial"}} contentEditable={editable}>{props.description}</p>
                 <div className="modal-row modal-content-row">
                 <div className="column modal-content-column">
                     <h3>Ingredients:</h3>
@@ -40,12 +70,12 @@ export default function Modal(props) {
                         {ingredientsArray}
                     </ul>
                     <h3>Cooking Instructions</h3>
-                    <p contentEditable="true">{props.instructions}</p>
+                    <p contentEditable={editable} id="instructions">{props.instructions}</p>
                     {/* <input id="instructions" value={props.instructions} disabled style={{width: "auto", height: "auto"}}></input> */}
                 </div>
                 <div className="column modal-content-column">
-                    <h3>Prep Time: <div contentEditable="true">{props.prepTime}</div></h3>
-                    <h3>Cook Time: <div contentEditable="true">{props.cookTime}</div></h3>
+                    <h3>Prep Time: <div contentEditable={editable} id="prep-time">{props.prepTime}</div></h3>
+                    <h3>Cook Time: <div contentEditable={editable} id="cook-time">{props.cookTime}</div></h3>
                     <h3>Attributes:</h3>
                     <div className="row" style={{gap: "0.5vw"}}>
                         {attributeArray}
@@ -53,6 +83,7 @@ export default function Modal(props) {
                 </div>
                 </div>
                 <button onClick={props.onClose} className="close-modal">Close</button>
+                <button onClick={() => handleSave()} className="close-modal" style={{display: "none"}}>Save</button>
             </div>
         </div>
     );
