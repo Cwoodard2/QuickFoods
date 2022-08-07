af42ab0b35f57859ff22bcafea97405eb04ae9d5
Cwoodard2 <cawooda@siue.edu>
2022-08-06 23:06:51 -0500

    Working to figure out how to make calendar work some ideas are in the works

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,27 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Calendar.scss";
 
-export default function Calendar() {
+export default function Calendar(props) {
+    const [details, setDetails] = useState("details-hidden");
+    const [recipeShow, setRecipeShow] = useState("");
+    const [recipeSet, setRecipeSet] = useState(false);
+    var dayOfWeek = props.day;
     var date = new Date();
     var getDay = date.getDay();
     var getDate = date.getDate();
     var getMonth = date.getMonth();
+    var recipe = {
+        Name: "Placeholder",
+        Prep: "1 min",
+        Cook: "2 min"
+    }
+
+    useEffect(() => {
+        const isRecipeSet = () => {
+            if (!recipeSet) {
+                setRecipeShow("calendar-item-hidden");
+            } else {
+                setRecipeShow("calendar-item");
+            }
+        }
+
+        isRecipeSet();
+    }
+    )
 
     const addRecipe = (whichMeal) => {
-        console.log(whichMeal);
+        setRecipeShow("calendar-item");
+        setRecipeSet(true);
         return "this is a word";
+    }
+
+    const showDetails = () => {
+        if (details === "details-hidden") {
+            setDetails("details");
+        } else {
+            setDetails("details-hidden");
+        }
     }
 
     return(
         <div>
             <div className="calendar-day">
-                <h3>{getDate} {getDay} {getMonth}</h3>
+                {/* <h3>{dayOfWeek}</h3> */}
                 <h4>Breakfast</h4>
-                <button onClick={() => addRecipe("breakfast")}>Add a recipe</button>
+                <div onClick={() => showDetails()} className={recipeShow}>
+                    <p>{recipe.Name}</p>
+                    <div className={details}>
+                        <p>{recipe.Prep}</p>
+                        <p>{recipe.Cook}</p>
+                    </div>
+                </div>
+                <button onClick={() => addRecipe("breakfast")} className="recipe-button">Add a recipe</button>
                 <h4>Lunch</h4>
-                <button onClick={() => addRecipe("lunch")}>Add a recipe</button>
+                <button onClick={() => addRecipe("lunch")} className="recipe-button">Add a recipe</button>
                 <h4>Dinner</h4>
-                <button onClick={() => addRecipe("dinner")}>Add a recipe</button>
+                <button onClick={() => addRecipe("dinner")} className="recipe-button">Add a recipe</button>
             </div>
         </div>
     )
--- src/pages/calender.js
+++ src/pages/calender.js
@@ -7,14 +7,25 @@ export default function Calender() {
     return(
         <StandardPage>
             <Navigation />
-            <div className="row" style={{justifyContent: "space-evenly", flexWrap: "wrap"}}>
-                <Calendar />
-                <Calendar />
-                <Calendar />
-                <Calendar />
-                <Calendar />
-                <Calendar />
-                <Calendar />
+            <div className="column">
+                <div className="row" style={{justifyContent: "space-evenly"}}>
+                    <h3>Sunday</h3>
+                    <h3>Monday</h3>
+                    <h3>Tuesday</h3>
+                    <h3>Wednesday</h3>
+                    <h3>Thursday</h3>
+                    <h3>Friday</h3>
+                    <h3>Saturday</h3>
+                </div>
+                <div className="row" style={{flexWrap: "wrap"}}>
+                <Calendar day="Sunady"/>
+                <Calendar day="Monday"/>
+                <Calendar day="Tuesday"/>
+                <Calendar day="Wednesday"/>
+                <Calendar day="Thursday"/>
+                <Calendar day="Friday"/>
+                <Calendar day="Saturday"/>
+            </div>
             </div>
         </StandardPage>
     );
